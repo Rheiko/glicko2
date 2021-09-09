358f077ff5ece461f92c0b5b4e65ad2435e3a0b8
Christian Rheinard Sadeko <37328751+Rheiko@users.noreply.github.com>
2021-09-09 22:36:11 +0700

    Add files via upload

---
+++ Glicko2ByRheiko/Glicko2ByRheiko/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.num_count = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txt_keterangan = New System.Windows.Forms.TextBox()
+        Me.p_rating = New System.Windows.Forms.NumericUpDown()
+        Me.p_rd = New System.Windows.Forms.NumericUpDown()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.p_vol = New System.Windows.Forms.NumericUpDown()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.num_count, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p_rating, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p_rd, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.p_vol, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(198, 18)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Masukkan Jumlah Lawan"
+        '
+        'num_count
+        '
+        Me.num_count.Location = New System.Drawing.Point(25, 30)
+        Me.num_count.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.num_count.Name = "num_count"
+        Me.num_count.Size = New System.Drawing.Size(120, 20)
+        Me.num_count.TabIndex = 2
+        Me.num_count.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(151, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Konfirmasi"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Player"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(22, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 18)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Rating"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(22, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Rating Deviation"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(233, 30)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(385, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(146, 18)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Input Data Lawan"
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Modern No. 20", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(25, 157)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(283, 31)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "C A L C U L A T E"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Location = New System.Drawing.Point(314, 30)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(293, 317)
+        Me.Panel1.TabIndex = 12
+        '
+        'txt_keterangan
+        '
+        Me.txt_keterangan.AcceptsReturn = True
+        Me.txt_keterangan.Location = New System.Drawing.Point(25, 223)
+        Me.txt_keterangan.Multiline = True
+        Me.txt_keterangan.Name = "txt_keterangan"
+        Me.txt_keterangan.ReadOnly = True
+        Me.txt_keterangan.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txt_keterangan.Size = New System.Drawing.Size(283, 124)
+        Me.txt_keterangan.TabIndex = 13
+        '
+        'p_rating
+        '
+        Me.p_rating.Location = New System.Drawing.Point(148, 78)
+        Me.p_rating.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.p_rating.Name = "p_rating"
+        Me.p_rating.Size = New System.Drawing.Size(120, 20)
+        Me.p_rating.TabIndex = 14
+        '
+        'p_rd
+        '
+        Me.p_rd.Location = New System.Drawing.Point(148, 105)
+        Me.p_rd.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.p_rd.Name = "p_rd"
+        Me.p_rd.Size = New System.Drawing.Size(120, 20)
+        Me.p_rd.TabIndex = 15
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 350)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(619, 22)
+        Me.StatusStrip1.TabIndex = 16
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(186, 17)
+        Me.ToolStripStatusLabel1.Text = "© 2021 Christian Rheinard Sadeko"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(22, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(147, 18)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Hasil Perhitungan"
+        '
+        'p_vol
+        '
+        Me.p_vol.DecimalPlaces = 2
+        Me.p_vol.Location = New System.Drawing.Point(148, 131)
+        Me.p_vol.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.p_vol.Name = "p_vol"
+        Me.p_vol.Size = New System.Drawing.Size(120, 20)
+        Me.p_vol.TabIndex = 19
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(22, 131)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(70, 18)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Volatility"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(619, 372)
+        Me.Controls.Add(Me.p_vol)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.p_rd)
+        Me.Controls.Add(Me.p_rating)
+        Me.Controls.Add(Me.txt_keterangan)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.num_count)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Glicko-2 Calculator"
+        CType(Me.num_count, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p_rating, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p_rd, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.p_vol, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents num_count As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents txt_keterangan As System.Windows.Forms.TextBox
+    Friend WithEvents p_rating As System.Windows.Forms.NumericUpDown
+    Friend WithEvents p_rd As System.Windows.Forms.NumericUpDown
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents p_vol As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ Glicko2ByRheiko/Glicko2ByRheiko/Form1.vb
@@ -0,0 +1,295 @@
+﻿Public Class Form1
+    '==========================================================================================================
+    '====================== "KALKULATOR GLICKO-2" BY CHRISTIAN RHEINARD SADEKO (2021) =========================
+    '==========================================================================================================
+    '=  == INTRO ==
+    '=  PROGRAM INI DIBUAT UNTUK MEMPERMUDAH PENGGUNA DALAM MENGHITUNG NILAI RATING MENGGUNAKAN KONSEP GLICKO-2
+    '=  SISTEM GLICKO-2 MERUPAKAN PENEMUAN PROFESOR MARK E. GLICKMAN DARI BOSTON UNIVERSITY, 20 NOVEMBER 2013
+    '=
+    '=  TERDAPAT 3 VARIABEL PENTING YANG DIGUNAKAN YAITU: RATING, RATING DEVIATION, DAN VOLATILITY
+    '=
+    '=  RUMUS DARI PROGRAM INI MENGIKUTI INSTRUKSI YANG DIJELASKAN DALAM JURNAL GLICKO-2 PADA:
+    '=  http://www.glicko.net/glicko/glicko2.pdf
+    '=  
+    '=  == DISCLAIMER ==
+    '=  KETEPATAN RUMUS TIDAK DIJAMIN PASTI, GUNAKAN DENGAN RESIKO YANG ANDA TANGGUNG SENDIRI
+    '=
+    '=  == CARA KERJA PROGRAM (END-USER) ==
+    '=  - TENTUKAN JUMLAH LAWAN DAN TEKAN TOMBOL KONFIRMASI
+    '=  - ISI SEMUA FIELD SESUAI KETERANGAN YANG ADA
+    '=  - TEKAN TOMBOL "CALCULATE" UNTUK MENGHITUNG PEROLEHAN NILAI RATING
+    '=
+    '=  == CARA KERJA PROGRAM (DEVELOPER) ==
+    '=  TOMBOL KONFIRMASI AKAN MEMICU FUNGSI PENAMBAHAN INPUT, ENABLE TOMBOL RESET, DISABLE TOMBOL KONFIRMASI
+    '=  FUNGSI PENAMBAHAN INPUT AKAN MEMBUAT KONTROL DINAMIS PADA FORM
+    '=  TOMBOL RESET AKAN MENGHAPUS SEMUA KONTROL DINAMIS YANG ADA, ENABLE TOMBOL KONFIRMASI, DISABLE TOMBOL RESET
+    '=  TOMBOL CALCULATE AKAN MEMICU PROSEDUR PERHITUNGAN GLICKO-2
+    '=  
+    '=  == DOWNLOAD ==
+    '=  https://github.com/Rheiko/glicko2
+    '=
+    '=  == END OF DOCUMENTATION ==
+    '=
+    '==========================================================================================================
+
+    '==========================================================================================================
+    '==========================================================================================================
+    '========== PERHITUNGAN GLICKO-2 !!!
+    '==========================================================================================================
+    '==========================================================================================================
+
+    Const t = 0.5
+    '==========================================================================================================
+    '========== DEKLARASI VARIABEL
+    '==========================================================================================================
+    Dim Rating(), RatDev(), Volat(), winlose() As Double 'NILAI YANG DIINPUT
+    Dim r(), rd(), v, delta, epsilon As Double 'NILAI YANG DIPROSES
+    Dim g(), exp(), A, B, C, fA, fB, fC, k, small_a As Double 'NILAI YANG DIPROSES
+
+    Sub GlickoCalc(ByVal count As Integer)
+        ReDim r(count), rd(count), g(count), exp(count) 'MENGUBAH UKURAN ARRAY DINAMIS
+        '=============================
+        '===== INISIALISASI AWAL =====
+        '=============================
+        v = 0
+        delta = 0
+        '===========================================================
+        '===== STEP 2 ===== KONVERSI RATING DAN RD KE SKALA GLICKO-2
+        '===========================================================
+        For i As Integer = 0 To count
+            r(i) = (Rating(i) - 1500) / 173.7178
+            rd(i) = RatDev(i) / 173.7178
+
+            If (i >= 1) Then
+                g(i) = 1 / Math.Sqrt(1 + 3 * Math.Pow(rd(i), 2) / Math.Pow(Math.PI, 2))
+                exp(i) = 1 / (1 + Math.Exp(-g(i) * (r(0) - r(i)))) 'r(0) = player | r(1..i) = opponents
+
+                '===============================================================================================
+                '===== STEP 3 ===== MENGHITUNG NILAI ESTIMASI VARIANTS DARI RATING PLAYER BERDASARKAN HASIL GAME
+                '===============================================================================================
+                v = v + Math.Pow(g(i), 2) * exp(i) * (1 - exp(i))
+                '=========================================================================================
+                '===== STEP 4 ===== MENGHITUNG NILAI DELTA ATAU ESTIMASI PERKEMBANGAN NILAI RATING DENGAN
+                '================== MEMBANDINGKAN NILAI RATING SEBELUM PERHITUNGAN (PRE-RATING PERIOD) 
+                '================== DENGAN RATING BERDASARKAN HASIL GAME
+                '=========================================================================================
+                delta = delta + g(i) * (winlose(i) - exp(i))
+            End If
+
+            'MessageBox.Show("r(" & i & ") = " & r(i).ToString & ", rd(" & i & ") = " & rd(i).ToString) '//DEBUG
+            'MessageBox.Show("g(" & i & ") = " & g(i).ToString & ", E(" & i & ") = " & exp(i).ToString) '//DEBUG
+        Next
+        v = Math.Pow(v, -1)
+        delta = delta * v
+
+        'MessageBox.Show("v = " & v.ToString & ", delta = " & delta.ToString) '//DEBUG
+
+        '========================================================
+        '===== STEP 5 ===== MENENTUKAN NILAI BARU DARI VOLATILITY
+        '========================================================
+        epsilon = 0.000001
+        small_a = Math.Log(Math.Pow(Volat(0), 2)) 'Math.Log10(Math.Pow(Volat(0), 2)) / Math.Log10(Math.E) 'ekuivalen ln(volat^2)
+        A = small_a
+
+        If Math.Pow(delta, 2) > Math.Pow(rd(0), 2) + v Then
+            B = Math.Log10((Math.Pow(delta, 2) - Math.Pow(rd(0), 2) - v)) / Math.Log10(Math.E)
+        ElseIf Math.Pow(delta, 2) <= Math.Pow(rd(0), 2) + v Then
+            k = 1
+            If compute(small_a - k * t) < 0 Then
+                k = k + 1
+            End If
+            B = small_a - k * t
+        End If
+        MessageBox.Show("A = " & A.ToString & ", B = " & B.ToString)
+        fA = compute(A)
+        fB = compute(B)
+        While (Math.Abs(B - A)) > epsilon
+            C = A + (A - B) * fA / (fB - fA)
+            fC = compute(C)
+            If fC * fB < 0 Then
+                A = B
+                fA = fB
+            Else
+                fA = fA / 2
+            End If
+            B = C
+            fB = fC
+        End While
+        MessageBox.Show("A = " & A.ToString & ", B = " & B.ToString)
+
+        Volat(0) = Math.Pow(Math.E, A / 2)
+
+        '======================================================================================
+        '===== STEP 6 ===== UPDATE NILAI RD KE NILAI RD SEBELUM PERHITUNGAN (PRE-RATING PERIOD)
+        '======================================================================================
+        rd(0) = Math.Sqrt(Math.Pow(rd(0), 2) + Math.Pow(Volat(0), 2)) 'RD pre-rating period
+
+        '===============================================================================
+        '===== STEP 7 ===== UPDATE NILAI RATING DAN RD KE NILAI BARU YANG TELAH DIHITUNG
+        '===============================================================================
+        rd(0) = 1 / Math.Sqrt((1 / Math.Pow(rd(0), 2)) + (1 / v)) 'RD Baru
+
+        Dim TempR As Double = 0
+        For i As Integer = 1 To count
+            TempR = TempR + (g(i) * (winlose(i) - exp(i)))
+        Next
+
+        r(0) = r(0) + Math.Pow(rd(0), 2) * TempR 'R Baru
+
+        '===================================================================
+        '===== STEP 8 ===== KONVERSI RATING DAN RD KEMBALI KE SKALA ORIGINAL
+        '===================================================================
+        r(0) = r(0) * 173.7178 + 1500
+        rd(0) = rd(0) * 173.7178
+
+        'MessageBox.Show("R = " & r(0).ToString & ", RD = " & rd(0).ToString) '//DEBUG
+    End Sub
+
+    Function compute(ByVal x As Double) As Double
+        Dim f, t As Double
+
+        f = ((Math.Pow(Math.E, x) * (Math.Pow(delta, 2) - Math.Pow(rd(0), 2) - v - Math.Pow(Math.E, x))) / (2 * Math.Pow((Math.Pow(rd(0), 2) + v + Math.Pow(Math.E, x)), 2))) - ((x - A) / Math.Pow(t, 2))
+        Return f
+    End Function
+
+    '==========================================================================================================
+    '========== FUNGSI PENAMBAHAN INPUT LAWAN
+    '==========================================================================================================
+    Dim cLeft As Integer = 1
+    Dim MyControls As List(Of Control)
+    Public Function AddNewTextBox()
+        Dim lbl_lawan As New System.Windows.Forms.Label()
+        Dim lbl_rating As New System.Windows.Forms.Label()
+        Dim lbl_rd As New System.Windows.Forms.Label()
+        Dim lbl_vol As New System.Windows.Forms.Label()
+        Dim lbl_winlose As New System.Windows.Forms.Label()
+        Dim txt_rating As New System.Windows.Forms.NumericUpDown
+        Dim txt_rd As New System.Windows.Forms.NumericUpDown
+        Dim txt_vol As New System.Windows.Forms.NumericUpDown
+        Dim cb_winlose As New System.Windows.Forms.CheckBox
+        With MyControls
+            .Add(lbl_lawan)
+            .Add(lbl_rating)
+            .Add(lbl_rd)
+            .Add(lbl_vol)
+            .Add(lbl_winlose)
+            .Add(txt_rating)
+            .Add(txt_rd)
+            .Add(txt_vol)
+            .Add(cb_winlose)
+        End With
+        With Panel1
+            .Controls.Add(lbl_lawan)
+            .Controls.Add(lbl_rating)
+            .Controls.Add(lbl_rd)
+            .Controls.Add(lbl_vol)
+            .Controls.Add(lbl_winlose)
+            .Controls.Add(txt_rating)
+            .Controls.Add(txt_rd)
+            .Controls.Add(txt_vol)
+            .Controls.Add(cb_winlose)
+        End With
+        lbl_lawan.Top = cLeft * 25 + ((cLeft - 1) * 125)
+        lbl_lawan.Left = 20
+        lbl_lawan.Text = "Lawan " & Me.cLeft.ToString
+        lbl_rating.Top = cLeft * 25 + 25 + ((cLeft - 1) * 125)
+        lbl_rating.Left = 20
+        lbl_rating.Text = "Rating"
+        lbl_rd.Top = cLeft * 25 + 50 + ((cLeft - 1) * 125)
+        lbl_rd.Left = 20
+        lbl_rd.Text = "Rating Deviation"
+        lbl_vol.Top = cLeft * 25 + 75 + ((cLeft - 1) * 125)
+        lbl_vol.Left = 20
+        lbl_vol.Text = "Volatility"
+        lbl_winlose.Top = cLeft * 25 + 100 + ((cLeft - 1) * 125)
+        lbl_winlose.Left = 20
+        lbl_winlose.Text = "Win?"
+        txt_rating.Top = cLeft * 25 + 25 + ((cLeft - 1) * 125)
+        txt_rating.Left = 150
+        txt_rating.Maximum = 10000
+        txt_rating.Name = "txt_rating" & Me.cLeft.ToString
+        txt_rd.Top = cLeft * 25 + 50 + ((cLeft - 1) * 125)
+        txt_rd.Left = 150
+        txt_rd.Maximum = 10000
+        txt_rd.Name = "txt_rd" & Me.cLeft.ToString
+        txt_vol.Top = cLeft * 25 + 75 + ((cLeft - 1) * 125)
+        txt_vol.Left = 150
+        txt_vol.DecimalPlaces = 2
+        txt_vol.Name = "txt_vol" & Me.cLeft.ToString
+        cb_winlose.Top = cLeft * 25 + 100 + ((cLeft - 1) * 125)
+        cb_winlose.Left = 150
+        cb_winlose.Name = "cb_winlose" & Me.cLeft.ToString
+        cLeft += 1
+        Return Panel1.Controls
+    End Function
+
+
+    '==========================================================================================================
+    '========== TOMBOL KONFIRMASI
+    '==========================================================================================================
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim JumlahLawan As Integer = num_count.Value
+        MyControls = New List(Of Control)
+        For i As Integer = 0 To JumlahLawan - 1
+            AddNewTextBox()
+        Next
+        Button1.Enabled = False
+        Button2.Enabled = True
+        Button3.Enabled = True
+    End Sub
+
+    '==========================================================================================================
+    '========== TOMBOL RESET
+    '==========================================================================================================
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        For Each c As Control In MyControls
+            Me.Controls.Remove(c)
+            c.Dispose()
+        Next
+        cLeft = 1
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Button3.Enabled = False
+    End Sub
+
+    '==========================================================================================================
+    '========== TOMBOL HITUNG
+    '==========================================================================================================
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Dim JumlahLawan As Integer = num_count.Value
+        ReDim Rating(JumlahLawan), RatDev(JumlahLawan), Volat(JumlahLawan), winlose(JumlahLawan)
+        Dim win As Integer = 0
+        Rating(0) = p_rating.Value
+        RatDev(0) = p_rd.Value
+        Volat(0) = p_vol.Value
+        For i As Integer = 1 To JumlahLawan
+            Dim txt_rating As NumericUpDown = Panel1.Controls.Item("txt_rating" & i)
+            Dim txt_rd As NumericUpDown = Panel1.Controls.Item("txt_rd" & i)
+            Dim txt_vol As NumericUpDown = Panel1.Controls.Item("txt_vol" & i)
+            Dim cb_winlose As CheckBox = Panel1.Controls.Item("cb_winlose" & i)
+            Rating(i) = txt_rating.Value
+            RatDev(i) = txt_rd.Value
+            Volat(i) = txt_vol.Value
+            If cb_winlose.Checked = True Then
+                win = 1
+            Else
+                win = 0
+            End If
+            winlose(i) = win
+        Next
+        txt_keterangan.AppendText("===== PERHITUNGAN =====" & Environment.NewLine &
+                                  "Rating Awal: " & Rating(0) & Environment.NewLine &
+                                  "RD Awal: " & RatDev(0) & Environment.NewLine &
+                                  "Volatility Awal: " & Volat(0) & Environment.NewLine
+                                  )
+        GlickoCalc(JumlahLawan)
+        txt_keterangan.AppendText("========================" & Environment.NewLine &
+                                  "Rating Akhir: " & r(0) & Environment.NewLine &
+                                  "RD Akhir: " & rd(0) & Environment.NewLine &
+                                  "Volatility Akhir: " & Volat(0) & Environment.NewLine & Environment.NewLine
+                                  )
+    End Sub
+
+
+
+End Class
